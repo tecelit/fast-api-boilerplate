990f201055f6f6b4bf32ed919fd1defa6f60a6ef
rtjshreyd <shreyanshece1041@gmail.com>
2023-10-24 06:08:46 +0530

    add token expiry time

--- svr/api/v1/users/auth.py
+++ svr/api/v1/users/auth.py
@@ -1,36 +1,59 @@
 from jose import JWTError, jwt
-from fastapi import Depends, HTTPException, status
+from fastapi import HTTPException, status
 from core.config import settings
 from database.models import User
-import bcrypt
 from fastapi.security import HTTPBearer
 from passlib.context import CryptContext
-from database.ops import users as user_ops
+from datetime import timedelta, datetime
+from passlib.exc import PasslibSecurityError
 
 ALGORITHM = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
 
 bearer_scheme = HTTPBearer()
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    try:
+        return pwd_context.hash(password)
+    
+    except PasslibSecurityError as e:
+        print(f"Error hashing password: {e}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Could not hash password")
+
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    try:
+        return pwd_context.verify(plain_password, hashed_password)
+    
+    except PasslibSecurityError as e:
+        print(f"Error verifying password: {e}")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+
 
 def create_jwt_token(user: User) -> str:
-    token_data = {
-        "sub": str(user.id), 
-        "role": user.role
-    }
-    return jwt.encode(token_data, settings.SECRET_KEY, algorithm=ALGORITHM)
+    try:
+        expiration_time = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        token_data = {
+            "sub": str(user.id), 
+            "role": user.role,
+            "exp": expiration_time
+        }
+    
+        token = jwt.encode(token_data, settings.SECRET_KEY, algorithm=ALGORITHM)
+        return token
+    
+    except JWTError as e:
+        print(f"Error encoding token: {e}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Could not create token")
+
 
 def decode_jwt_token(token: str) -> dict:
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[ALGORITHM])
         return payload
+    
     except JWTError as e:
         print(f"Error decoding token: {e}")
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
-
--- svr/core/config.py
+++ svr/core/config.py
@@ -21,6 +21,7 @@ config_vars = {
 class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
     # Other settings...
 
 settings = Settings()
